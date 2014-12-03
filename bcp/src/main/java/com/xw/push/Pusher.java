7af20e6a83a6e714377be7158b85a21d69fdbab4
Junjie Mars <junjiemars@gmail.com>
2014-12-03 09:41:48 +0800

    supports logger mount

--- bcp/src/main/java/com/xw/push/Pusher.java
+++ bcp/src/main/java/com/xw/push/Pusher.java
@@ -11,8 +11,6 @@ import com.baidu.yun.channel.model.PushUnicastMessageResponse;
 import com.baidu.yun.core.log.YunLogEvent;
 import com.baidu.yun.core.log.YunLogHandler;
 
-import static java.lang.System.out;
-
 /**
  * Just one static class for pushing.
  * @author junjiemars@mail.xwtec.cn
@@ -28,6 +26,7 @@ public final class Pusher {
      * @param channelId 推送通道ID，通常指一个终端，如一台android系统手机。客户端绑定调用返回值中可获得
      * @param messageType 消息类型
      * @param content 要推送的消息体内容
+     * @param logger 日志处理器, 可为null
      * @return 成功后返回:>0, 失败后返回:0
      */
     public static final int push_unicast_message(
@@ -42,7 +41,7 @@ public final class Pusher {
 
         final BaiduChannelClient client = _build_channel_client(
                 apiKey, secretKey);
-        _set_logger(client, logger);
+        _mount_logger(client, logger);
         final PushUnicastMessageRequest request = _build_unicast_request(
                 deviceType,
                 channelId,
@@ -56,10 +55,9 @@ public final class Pusher {
             final int c = response.getSuccessAmount();
             return (c);
         } catch (final ChannelClientException e) {
-            logger.log_client_exception(e.getMessage());
+            _mount_logger(logger, e);
         } catch (final ChannelServerException e) {
-            logger.log_channel_exception(e.getRequestId(),
-                    e.getErrorCode(), e.getMessage());
+            _mount_logger(logger, e);
         }
 
         return (RETURN_FAILED);
@@ -71,7 +69,8 @@ public final class Pusher {
      * @param secretKey 应用私钥，服务端推送消息时用到
      * @param deviceType 设备类型标识
      * @param messageType 消息类型标识
-     * @param content 要推送的消息体内容
+     * @param content 要推送的消息体内容，需要按规范构建
+     * @param logger 日志处理器, 可为null
      * @return 成功后返回:>0, 失败后返回:0
      */
     public static final int push_broadcast_message(
@@ -84,7 +83,7 @@ public final class Pusher {
 
         final BaiduChannelClient client = _build_channel_client(
                 apiKey, secretKey);
-        _set_logger(client, logger);
+        _mount_logger(client, logger);
         final PushBroadcastMessageRequest request = _build_broadcast_request(
                 deviceType,
                 messageType,
@@ -97,10 +96,9 @@ public final class Pusher {
             final int c = response.getSuccessAmount();
             return (c);
         } catch (final ChannelClientException e) {
-            logger.log_client_exception(e.getMessage());
+            _mount_logger(logger, e);
         } catch (final ChannelServerException e) {
-            logger.log_channel_exception(
-                    e.getRequestId(), e.getErrorCode(), e.getMessage());
+            _mount_logger(logger, e);
         }
 
         return (RETURN_FAILED);
@@ -146,15 +144,41 @@ public final class Pusher {
         return (c);
     }
 
-    private static final void _set_logger(
+    private static final void _mount_logger(
             final BaiduChannelClient c,
             final Logger logger) {
+        if (null == logger) {
+            return;
+        }
+
         c.setChannelLogHandler(new YunLogHandler() {
             public void onHandle(YunLogEvent event) {
                 logger.log_channel(event.getMessage());
             }
         });
     }
+
+    private static final void _mount_logger(
+            final Logger logger,
+            final ChannelClientException e) {
+        if (null == logger) {
+            return;
+        }
+
+        logger.log_client_exception(e.getMessage());
+    }
+
+    private static final void _mount_logger(
+            final Logger logger,
+            final ChannelServerException e) {
+        if (null == logger) {
+            return;
+        }
+
+        logger.log_channel_exception(e.getRequestId(),
+                e.getErrorCode(), e.getMessage());
+    }
+
 
     private static final int RETURN_FAILED = (0);
 }
